f89353b2bd49477eb5515c7e2b077b85ea56431c
David Rivera A <davidriveraarbelaez@gmail.com>
2025-04-11 19:45:40 -0500

    ContextAPI

--- Unidad_3/ContextAPI/ContextAPI/src/contexts/AppContext.tsx
+++ Unidad_3/ContextAPI/ContextAPI/src/contexts/AppContext.tsx
@@ -1,1 +1,31 @@
+import React, { createContext, useState, FC, ReactNode} from 'react'
 
+interface AppContextInterface {
+    count: number;
+    increase: () => void;
+    decrease: () => void;
+}
+
+export const AppContext = createContext<AppContextInterface | null>(null)
+
+interface AppProviderProps {
+    children: ReactNode;
+}
+
+export const AppProvider: FC<AppProviderProps> = ({ children }) => {
+    const [count, setCount] = useState(0)
+
+    const increase = () => {
+        setCount(count + 1)
+    }
+
+    const decrease = () => {
+        setCount(count - 1)
+    }
+
+    return (
+        <AppContext.Provider value={{ count, increase, decrease }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
